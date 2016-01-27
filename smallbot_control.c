1fa0a158a385dd69138ddcc3aaeedd512cb0fb30
Ethan Stewart <ethan@atdllc.com>
2016-01-26 19:22:07 -0500

    Added more stuff

--- smallbot_control.c
+++ smallbot_control.c
@@ -1,6 +1,13 @@
 #include "smallbot_control.h"
 
 void motor_set(int left, int right){
+	motor(RightFront)  = right;
+	motor(RightMiddle) = right;
+	motor(RightRear)   = right;
+
+	motor(LeftFront)  = left;
+	motor(LeftMiddle) = left;
+	motor(LeftRear)   = left;
 }
 
 void intake_set(int speed){
