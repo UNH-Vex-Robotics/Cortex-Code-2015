4992e9c3462ea74160e2e0db440a07e7d4eb2d46
Ethan Stewart <ethan@atdllc.com>
2016-01-29 01:30:10 -0500

    Did work on largebot stuff, built off of mikec's bigbot.c

--- largebot_control.c
+++ largebot_control.c
@@ -1,11 +1,40 @@
+int winch_get_encoder(){ return SensorValue(WinchEncoder); }
 
-void winch_set(int speed){
-	motor(LeftWinch)  = speed;
-	motor(RightWinch) = speed;
+int winch_at_top(){ return SensorValue(WinchLimitSwitch); }
+
+
+int smallbot_dist(){ return SensorValue(LiftSonar); }
+
+int is_ball_ready(){
+	return SensorValue(BallLightSensor) < BALL_EXISTS_THRESH;
 }
 
-int winch_get_encoder(){ return SensorValue(WinchEncoder);}
 
-int limit_switch_get(){ return SensorValue(WinchLimitSwitch);}
+void intake_set(int speed){
+	motor[IntakeFunnel] = speed;
+}
 
-int small_bot_detector(){ return SensorValue(SmallBotDetector);}
+void belt_set(int speed){
+	motor[Belt] = speed;
+}
+
+void shooter_motor_set(int speed){
+	motor[FlyRight1] = speed;
+	motor[FlyRight2] = speed;
+	motor[FlyRight3] = speed;
+
+	motor[FlyLeft1] = speed;
+	motor[FlyLeft2] = speed;
+	motor[FlyLeft3] = speed;
+}
+
+void winch_set(int speed){
+	motor[LeftWinch]  = speed;
+	motor[RightWinch] = speed;
+}
+
+int smallbot_lift_ready(){
+	int dist = smallbot_dist()
+	return dist > SMALLBOT_CORRECT_PLACEMENT_MIN &&
+	       dist < SMALLBOT_CORRECT_PLACEMENT_MAX;
+}
--- largebot_control.h
+++ largebot_control.h
@@ -11,10 +11,26 @@ const int WINCH_UP_SPEED   =  127;
 const int WINCH_OFF_SPEED  =  0;
 const int WINCH_DOWN_SPEED = -127;
 
+const int SHOOTER_SPEED = 90;
+const int SHOOTER_OFF_SPEED = 90;
+
+// if the ball-detecting light sensor reads below this value, we consider it a ball
+const int BALL_EXISTS_TRESH = 400;
+
+// CALIBRATEME
+const int SMALLBOT_CORRECT_PLACEMENT_MIN = 0;
+const int SMALLBOT_CORRECT_PLACEMENT_MAX = 0;
 
 void winch_set(int speed);
 int winch_get_encoder();
+int winch_at_top();
 
-int limit_switch_get();
+int smallbot_lift_ready();
+int smallbot_dist();
 
-int small_bot_detector();
+int is_ball_ready();
+
+void intake_set(int speed);
+void belt_set(int speed);
+
+void shooter_motor_set(int speed);
