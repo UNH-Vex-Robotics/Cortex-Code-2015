08991e8ce6073e9db73faae7ed8bf95b08ff6694
jemills333 <jemills333@gmail.com>
2016-01-29 22:01:38 -0500

    worked on autonomous

--- smallbot_control.h
+++ smallbot_control.h
@@ -16,9 +16,9 @@ const int PUSHER_ENC_DIST_MAX    = 983;
 const int PUSHER_ENC_DIST_THRESH = 970;
 
 // speed defines for vairous subsystems/actions
-const int SUCKER_OUT_SPEED = 127;
-const int SUCKER_OFF_SPEED = 0;
-const int SUCKER_IN_SPEED  = -127;
+const int INTAKE_OUT_SPEED = 127;
+const int INTAKE_OFF_SPEED = 0;
+const int INTAKE_IN_SPEED  = -127;
 
 const int PUSHER_OUT_SPEED = 127;
 const int PUSHER_OFF_SPEED = 0;
@@ -40,3 +40,21 @@ int pusher_get_home_switch();
 
 int bumperswitch_get_left();
 int bumperswitch_get_right();
+
+const int CIRC_D = 10.375; //in inches
+
+float drive_encoder_to_inches (int encoder_value){
+	return ((CIRC_D*encoder_value)/360.0);
+}
+
+int inches_to_drive_encoder (float inches){
+	return ((inches*360.0)/CIRC_D);
+}
+
+float drive_encoder_to_degrees (int encoder_value){
+	return (encoder_value*(360/1308));
+}
+
+int degrees_to_drive_encoder (float degrees_value){
+	return (degrees_value*(1308/360));
+}
