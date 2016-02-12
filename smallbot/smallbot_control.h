1ab7957bc0b33962b4e9cbfdd24781dcfbc1e3b9
Ethan Stewart <ethan@atdllc.com>
2016-02-12 18:29:04 -0500

    Changed driving forward to use the gyro instead of encoders, it is untestsed.

--- smallbot/smallbot_control.h
+++ smallbot/smallbot_control.h
@@ -14,33 +14,42 @@ const int LINE_LRM    = 7;
 const unsigned int LINE_THRESH = 700;
 
 // miliseconds it takes to dump all balls out
-const int DUMP_BALL_LOW_INTAKE_TIME = 1500;
+const int DUMP_BALL_LOW_INTAKE_TIME  = 1500;
 const int DUMP_BALL_HIGH_INTAKE_TIME = 1500;
 
 // speed defines for vairous subsystems/actions
-const int INTAKE_IN_SPEED = 127;
+const int INTAKE_IN_SPEED  = 127;
 const int INTAKE_OFF_SPEED = 0;
-const int INTAKE_OUT_SPEED  = -127;
+const int INTAKE_OUT_SPEED = -127;
 
 const int TOP_INTAKE_OUT_SPEED = 127;
 const int TOP_INTAKE_OFF_SPEED = 0;
 const int TOP_INTAKE_IN_SPEED  = -127;
 
+const int DRIVE_WHEEL_CIRC = 10.375; //in inches
+
+const float GYRO_TICKS_PER_DEGREE = 10.0;
+
 // Various robotly functions
 void motor_set(int right, int left);
-
-void intake_set(int speed);
-void top_intake_set(int speed);
 
 int motor_get_left_encoder();
 int motor_get_right_encoder();
 
+float drive_encoder_to_inches(int encoder_value);
+int   inches_to_drive_encoder(float inches);
+float drive_encoder_to_degrees(int encoder_value);
+int   degrees_to_drive_encoder(float degrees_value);
+
+
+void intake_set(int speed);
+void top_intake_set(int speed);
+
+int   get_gyro();
+float get_gyro_degrees();
+int   gyro_degrees_to_ticks(float deg);
+float gyro_ticks_to_degrees(int ticks);
+
 int bumperswitch_get_left();
 int bumperswitch_get_right();
 
-const int DRIVE_WHEEL_CIRC = 10.375; //in inches
-
-float drive_encoder_to_inches (int encoder_value);
-int inches_to_drive_encoder (float inches);
-float drive_encoder_to_degrees (int encoder_value);
-int degrees_to_drive_encoder (float degrees_value);
