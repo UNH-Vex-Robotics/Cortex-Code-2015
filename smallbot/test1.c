54a975fbce905d4bd7bc82350740d7fb8a90cd80
Ethan Stewart <ethan@atdllc.com>
2016-02-22 19:35:02 -0500

    example code... 2

--- smallbot/test1.c
+++ smallbot/test1.c
@@ -5,28 +5,22 @@
 //
 // Simple program to test user c<t_8b>trol over the VEX Cortex UART ports. 
 //
-// 1. The two ports are looped back
--
-to
--
-back.
+// 1. The two ports are looped back - to - back.
 //
-// 2. One port sim
-ply outputs characters with hex values 0, 1, 2, ... 255, 0, 1, 2, ...
+// 2. One port sim ply outputs characters with hex values 0, 1, 2, ... 255, 0, 1, 2, ...
 //
 // 3. The "UARTReceive" task grabs the characters from the receive UART and verifies that they are
 //    in the above sequence.
 //
-// Various peg counts are used to accumulate total c
-haracters transmitted, received and "out of
+// Various peg counts are used to accumulate total c haracters transmitted, received and "out of
 // sequence" counts. These can be viewed in the ROBOTC debugger.
 //
 // NOTE: Internally the firmware uses interrupt handlers to buffer the characters transmitted and
-//       received. The buffers are about 25 ch
-aracters in length. If user program does not extract
+//       received. The buffers are about 25 ch aracters in length. If user program does not extract
 //       the characters fast enough the older characters are simply discarded.
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////////
+
 task UARTReceive();
 int nRcvIndex = 0;
 long nTotalXmitChars = 0;
@@ -37,18 +31,16 @@ int rcvChar;
 unsigned char rcvChars[23];
 
 // Keep buffer of last 23 characters received.
-task main()
-{
-	// Setup the two UA
-	RT ports
-		configureSerialPort(uartOne, uartUserControl);
+task main() {
+	// Setup the two UA RT ports
+	configureSerialPort(uartOne, uartUserControl);
 	configureSerialPort(uartTwo, uartUserControl);
 	setBaudRate(uartOne, baudRate115200);
-	setBaudRate(uartTwo, baudRate115200);
+	// setBaudRate(uartTwo, baudRate115200);
 	while (getChar(uartTwo) != -1)
 		// Purge existing chars from buffer
 	{}
-	StartTask(UARTReceive);
+	// StartTask(UARTReceive);
 	while (true) {
 		// Loop forever transmitting the characters 0, 1, 2, ..., 255, 0, 1, 2, ....
 		++nTotalXmitChars;
@@ -60,8 +52,8 @@ task main()
 		}
 	}
 }
-task UARTReceive()
-{
+
+task UARTReceive() {
 	while (true)
 	{
 		// Loop forever getting characters from the "receive" UART. Validate that they arrive in the expected
